ae64a6e46011b5978d88ff2c05acaa18e7668d8e
Ubuntu <bhavinshah.work@gmail.com>
2021-01-10 23:20:45 +0000

    ssh push

--- HelloJava.java
+++ HelloJava.java
@@ -2,7 +2,7 @@ class HelloJava {
 
 public static void main(String args{}) {
 
-System.out.Println("I am your Java Program. Thank you");
+System.out.Println("I am your Java Program. Thank you  ssh key push");
 
 }
 
